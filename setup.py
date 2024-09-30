96145dd4fa42b88418e361065bd2ff8a862163f0
rustadadam <rustadadam@gmail.com>
2024-09-30 13:40:54 -0600

    Update setup

--- setup.py
+++ setup.py
@@ -2,16 +2,24 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='SPUD_and_MASH',   # Name of your package
+    name='mashspud',   # Name of your package
     version='0.1',          # Version number
     packages=find_packages(), # Automatically find sub-packages
     description='Manifold Alignment Methods',
-    #author='Your Name',
+    author='Adam G. Rustad',
     #author_email='your.email@example.com',
     url='https://github.com/rustadadam/SPUD_and_MASH.git',  # URL for the package (if applicable)
     include_package_data=True,  # To include non-Python files if you have any
     install_requires=[  # List of dependencies
-        # Add any dependencies like 'numpy', 'pandas', etc.
+        "scipy",
+        "graphtools",
+        "numpy",
+        "matplotlib",
+        "igraph",
+        "sklearn",
+        "time",
+        "seaborn",
+        "pandas"
     ],
     classifiers=[
         'Programming Language :: Python :: 3',
